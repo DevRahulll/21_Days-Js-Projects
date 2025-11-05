8526117d0ce092ad37b4679f533da423fc3f62a6
Rahul <rahuldevan2019@gmail.com>
2025-11-05 23:26:31 +0530

    finished project 3 Digital Clock

--- 03_digital_clock/script.js
+++ 03_digital_clock/script.js
@@ -1,4 +1,16 @@
-let currentTime=new Date();
-let time=currentTime.getMinutes();
+let h = document.querySelector("#hour")
+let m = document.querySelector("#minute")
+let s = document.querySelector("#second")
 
-console.log(time);
+setInterval(function setTime() {
+    let time = new Date();
+    h.textContent = time.getHours();
+    m.textContent = time.getMinutes();
+    s.textContent = time.getSeconds();
+}, 1000)
+
+
+
+
+
+
